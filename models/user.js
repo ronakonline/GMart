883bcffb98469f97ae2692ce5817a8623808d127
ashu1907 <rajputashhtosh@gmail.com>
2021-05-24 13:56:52 +0530

    added schemas

--- models/user.js
+++ models/user.js
@@ -2,10 +2,48 @@ var mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
+    id: {
+      type: Number,
+      required: true,
+    },
     email: {
       type: String,
       required: true,
     },
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    created_at: {
+      type: Date,
+      dafault: Date.now,
+    },
+    updated_at: {
+      type: Date,
+      default: Date.now,
+    },
+    last_login: {
+      type: Date,
+      default: Date.now,
+    },
+    active: {
+      type: Boolean,
+    },
+    status: {
+      type: Boolean,
+    },
+    code_varification: {
+      type: String,
+    }
+
   },
   { timestamps: true }
 );
